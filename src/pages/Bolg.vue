d43c8526cdd2a8b4d74b0aea72a9c6335f29909f
MrGxsheng <2838428014@qq.com>
2023-08-27 19:23:29 +0800

    修改ui

--- src/pages/Bolg.vue
+++ src/pages/Bolg.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="row justify-center" style="max-width: 100%;">
-    <!--   左一   -->
-    <div class="col-12 col-md-1"/>
-    <!--   中10   -->
-    <div class="col-12 col-md-10" style="color: azure;">
-<!--      <div class="html" v-html="page"></div>-->
+  <div class="q-pa-md q-gutter-sm">
+    <!--   博客    -->
+    <q-card class="shadow-10 q-ma-lg blog-container">
+      <q-card-section style="font-size: 2em">
+        {{ noteName }}
+      </q-card-section>
+      <q-img :src=imgPath style="margin-bottom: 66px;"/>
+
       <v-md-preview :text="page"/>
 
-    </div>
-    <!--   右一   -->
-    <div class="col-12 col-md-1"/>
+    </q-card>
   </div>
-
 </template>
 
 <script setup>
@@ -44,6 +43,8 @@ app.use(VMdPreview);
 const $router = useRouter()
 const page = ref("")
 const title = ref("")
+const noteName = ref(localStorage.getItem("noteName"))
+const imgPath = ref(localStorage.getItem("imgPath"))
 {
   let id = localStorage.getItem("textId")
   if (id === null) {
@@ -58,10 +59,100 @@ const title = ref("")
 </script>
 
 <style scoped>
-.html {
-  max-width: 100%;
-  margin-left: auto;
-  margin-right: auto;
+
+._h {
+  font-weight: bold;
+  border-bottom: solid rgba(255, 255, 255, .6) 1px;
+  line-height: 1.2;
 }
 
+.blog-h1 {
+  font-size: 2.3em;
+}
+
+.blog-h2 {
+  font-size: 2.2em;
+}
+
+.blog-h3 {
+  font-size: 2.1em;
+}
+
+.blog-h4 {
+  font-size: 2.0em;
+}
+
+.blog-h5 {
+  font-size: 1.9em;
+}
+
+.blog-h6 {
+  font-size: 1.8em;
+}
+
+.code-field {
+  background-color: rgba(255, 255, 255, .29);
+}
+
+.blog-container {
+  overflow: hidden;
+  /*position: absolute;*/
+  /*left: 350px;*/
+  /*width: 50%;*/
+  padding-bottom: 30px;
+  background-color: rgba(255, 255, 255, .3);
+}
+
+.barBtn {
+  margin-right: 3%;
+  min-width: 100px;
+}
+
+.a {
+  transition: .35s ease-in-out;
+}
+
+.a:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+  color: #ec85a7;
+}
+
+a {
+  text-decoration: none;
+  color: #1D1D1D;
+}
+
+.img-to-big {
+  transition: .35s ease-in-out;
+}
+
+.img-to-big:hover {
+  transform: scale(1.2);
+}
+
+.blog-link {
+  text-decoration: none;
+  font-size: 1.6em;
+  color: #0380f6;
+  transition: .25s ease-in-out;
+}
+
+.blog-link:hover {
+  color: #ec85a7;
+  background-color: rgba(255, 255, 255, .1);
+}
+
+/*引用*/
+._cite {
+  border-left: 4px solid #f19ab8;
+  padding: 0 15px;
+  color: #ec85a7;
+  background-color: rgba(255, 255, 255, 0.66);
+  line-height: 2;
+}
+
+._base {
+  margin: 10px 10px 3px;
+  width: 98%;
+}
 </style>
